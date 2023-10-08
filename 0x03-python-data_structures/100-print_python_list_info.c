5186e142848db5fad48716b59ba2d4ef331267ac
Wannero <silver-79@live.fr>
2023-10-08 04:38:15 -0700

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,21 +1,29 @@
-import ctypes
+#include <Python.h>
+#include <stdio.h>
+/**
+ * print_python_list_info - print python
+ * @p: input
+ */
+void print_python_list_info(PyObject *p)
+{
+	unsigned int l_idx;
+	unsigned int len;
+	unsigned int allocated;
+	PyTypeObject *type;
+	const char *name;
 
-lib = ctypes.CDLL('./libPyList.so')
-lib.print_python_list_info.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
-lib.print_python_list_info(l)
-del l[1]
-lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
-lib.print_python_list_info(l)
-l = []
-lib.print_python_list_info(l)
-l.append(0)
-lib.print_python_list_info(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list_info(l)
-l.pop()
-lib.print_python_list_info(l)
+	if (p == NULL)
+		return;
+
+	len = (unsigned int) PyList_Size(p);
+	allocated = (unsigned int) ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %d\n", len);
+	printf("[*] Allocated = %d\n", allocated);
+
+	for (l_idx = 0; l_idx < len; l_idx++)
+	{
+		type = PyList_GET_ITEM(p, l_idx)->ob_type;
+		name = type->tp_name;
+		printf("Element %d: %s\n", l_idx, name);
+	}
+}
